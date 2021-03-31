38b5913197658e04d52cceac0d1020f2a7cd60b7
CPSLDD <cpslddzl@163.com>
2021-03-31 20:45:34 +0800

    test

---
+++ test-3-26/test-3-26/test-3-26.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+//
+//int check_sys()
+//{
+//	//判断大小端
+//	int a = 1;
+//	char* p = (char*)&a;
+//	return *p;
+//}
+//
+//int main()
+//{
+//	int ret = check_sys();
+//	if (ret == 1)
+//	{
+//		printf("小端\n");
+//	}
+//	else
+//	{
+//		printf("大端\n");
+//	}
+//}
