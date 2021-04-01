c5ec024c8e6b79aaf892ee12b8812f063da3bee8
CPSLDD <cpslddzl@163.com>
2021-04-01 10:36:34 +0800

    操作符学习

---
+++ 操作符/操作符/test-3-23.cpp
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	double a = 5 / 2.0; //左操作数与右操作数有浮点数，做浮点数除法
+//	printf("a=%lf", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 16;
+//	int b = a >> 1;
+//	printf("b=%d", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3; //011	
+//	int b = 5; //101
+//	int c = a & b;//001
+//	printf("c=%d", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 100;
+//	int b = 325;
+//	printf("a=%d,b=%d\n",a, b);
+//	a = a ^ b;
+//	b = b ^ a;
+//	a = a ^ b;
+//	printf("a=%d,b=%d\n",a, b);
+//	return 0;
+//}
+
+//int main()//求数字二进制后含1的数量
+//{
+//	int num = 0;
+//	int count = 0;
+//	scanf("%d", &num);
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if(1==((num>>i)&1))
+//			count++;
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+
+//创建结构体
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//	char id[20];
+//};
+//
+//int main()
+//{
+//	int a = 10;
+//	//使用struct Stu这个类型创建了一个学生对象s1，并初始化
+//	struct Stu s1 = { "张三",20,"2019010305" };
+//	return 0;
+//}
+
